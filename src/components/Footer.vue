3947b3af03ad368411997519c19aee08199a5e63
Herman Ling <hermanling1@gmail.com>
2025-06-14 13:16:18 +0800

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,8 @@
     <footer class="bg-light text-center py-3 mt-auto">
         <div class="container">
             <small>&copy; 2025 VivaPC. All rights reserved.</small>
+            <br>
+            <small class="text-muted">For educational purposes only.</small>
         </div>
     </footer>
 </template>
